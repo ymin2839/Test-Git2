70b1e7645181581379d6a3f417931b3a7fdb4c7a
ymin <ymin2839@gmail.com>
2021-04-06 13:50:51 +0900

    Revert "이전으로 돌아가보는거"
    This reverts commit 54b5df20bbc17dedee2d87c0e49b62efbb42cc82.

--- Test/TestDlg.cpp
+++ Test/TestDlg.cpp
@@ -30,7 +30,6 @@ BOOL CTestDlg::OnInitDialog()
 	AfxMessageBox(L"마스터:되돌리기 했더니 이전으로 돌아갔음");
 	AfxMessageBox(L"마스터:다시설정 변경내용 유지 했더니 변경내용으로 나왔음 아마 내용 삭제 했으면 윗줄 없어 질듯");
 	AfxMessageBox(L"마스터:다시설정 했더니 같은 소스 변경 되서 그런지 병합나왔음");
-	AfxMessageBox(L"마스터:이 줄을 지워 볼꺼임");
 
 	return TRUE;
 }
